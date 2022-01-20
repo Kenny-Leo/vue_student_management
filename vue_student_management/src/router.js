c561892e1f6a9bea6640d835fc1b2ec8bf7a5d6f
Kenny-Leo <97043946+Kenny-Leo@users.noreply.github.com>
2022-01-20 15:21:18 +0800

    Add files via upload

---
+++ vue_student_management/src/router.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+import Welcome from './components/Welcome.vue'
+import AddStu from './components/user/AddStu.vue'
+import StuList from './components/user/StuList.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/AddStu', component: AddStu },
+        { path: '/StuList', component: StuList }
+      ]
+    }
+  ]
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //     next()  放行    next('/login')  强制跳转
+
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router
